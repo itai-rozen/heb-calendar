68741326dcae0f21ea56fc77e142add6583b18c3
itai rozen <itai.rozen@mail.huji.ac.il>
2021-05-03 14:06:01 +0300

    fixed server problems

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ app.use(cors())
 const port = process.env.PORT || 5000
 const reminders = require('./routes/api/reminders.js')
 app.use('/api/reminders', reminders)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(__dirname + '/public/'))
+
+    app.get('/.*/',(req,res) => res.sendFile(__dirname + '/public/index.html'))
+}
 app.listen(port, () => console.log(`server listens on ${port}`))
 
 
