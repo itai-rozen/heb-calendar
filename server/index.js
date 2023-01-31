d491eff6c70207eca4dd20c910407601bc095be4
itai rozen <itai.rozen@mail.huji.ac.il>
2023-01-31 19:17:28 +0200

    removed bodyparser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({extends: true}))
 app.use(cors())
 
 const port = process.env.PORT || 5000
